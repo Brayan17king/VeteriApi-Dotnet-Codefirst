96c97cff1f8ce74fb1fb4f3afed7031e0b4300df
Brayan17king <xbrayansneyder@gmail.com>
2023-10-02 20:54:50 -0500

    feat: :sparkles: we transform classes into entities

--- Infrastructure/Data/AnimalsContext.cs
+++ Infrastructure/Data/AnimalsContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data; // facilita el proceso de llamado de la clase
@@ -12,6 +13,17 @@ public class AnimalsContext : DbContext
     public AnimalsContext(DbContextOptions options) : base(options)
     {
     }
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Cita> Citas { get; set; }
+    public DbSet<Ciudad> Ciudades { get; set; }
+    public DbSet<ClienteDireccion> ClienteDirecciones { get; set; }
+    public DbSet<ClienteTelefono> ClienteTelefonos { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Mascota> Mascotas { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Raza> Razas { get; set; }
+    public DbSet<Servicio> Servicios { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
