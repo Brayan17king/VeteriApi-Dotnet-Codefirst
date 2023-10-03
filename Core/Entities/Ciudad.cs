83584954f20130b4717cf93b9a67137422c7eaa8
Brayan17king <xbrayansneyder@gmail.com>
2023-10-02 20:04:14 -0500

    feat: :sparkles: We define the attributes of the entities

--- Core/Entities/Ciudad.cs
+++ Core/Entities/Ciudad.cs
@@ -6,5 +6,8 @@ using System.Threading.Tasks;
 namespace Core.Entities;
 public class Ciudad : BaseEntity
 {
-
+    public int IdDepartamento { get; set; }
+        public Departamento Departamentos { get; set; }
+        public ICollection<Cliente> Clientes { get; set; }
+        public ClienteDireccion ClienteDireccion { get; set; } 
 }
