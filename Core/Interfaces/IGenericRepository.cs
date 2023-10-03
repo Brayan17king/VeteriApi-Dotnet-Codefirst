c481e81c717698a7ae09ef784cb2eb111f6ab551
Brayan17king <xbrayansneyder@gmail.com>
2023-10-03 12:59:56 -0500

    feat: :sparkles: Creation of Interfaces with their respective methods

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> GetByIdAsync(int Id);
+    Task<IEnumerable<T>> GetAllAsync();
+    IEnumerable<T> Find(Expression<Func<T, bool>> expression); //Busquedas apartir de expresiones Linq
+    Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, string search); //segmentar la cantidad de registros 
+    void Add(T entity); //agregar 
+    void AddRange(IEnumerable<T> entities);
+    void Remove(T entity); // Remover
+    void RemoveRange(IEnumerable<T> entities);
+    void Update(T entity); //Actualizar
+}
