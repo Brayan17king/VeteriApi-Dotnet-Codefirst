d9ba32d2c373b754279d1ad4adfb7ef7539005bb
Brayan17king <xbrayansneyder@gmail.com>
2023-10-02 19:24:59 -0500

    feat: :sparkles: Connection injection

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AnimalsContext>(optionsBuilder =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
