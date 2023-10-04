d4efd612be97f49c60ea01330efcd0ab746442d0
Brayan Martinez <104658332+Brayan17king@users.noreply.github.com>
2023-10-04 12:35:02 +0000

    feat: :sparkles: creation of generic repositories with their respective methods

--- Infrastructure/Repositories/GenericRepository.cs
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -1,68 +1,80 @@
-
 using System.Linq.Expressions;
 using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Repositories;
-
-public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+namespace Infrastructure.Repositories
 {
-    private readonly AnimalsContext _context;
-
-    public GenericRepository(AnimalsContext context)
+    public class GenericRepository<T> : IGenericRepository<T>
+        where T : BaseEntity
     {
-        _context = context;
-    }
+        private readonly AnimalsContext _context;
 
-    public virtual void Add(T entity)
-    {
-        _context.Set<T>().Add(entity);
-    }
+        public GenericRepository(AnimalsContext context)
+        {
+            _context = context;
+        }
 
-    public virtual void AddRange(IEnumerable<T> entities) //virtual sirve para sobrecargar los metodos
-    {
-        _context.Set<T>().AddRange(entities);
-    }
+        public virtual void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
 
-    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
-    {
-        return _context.Set<T>().Where(expression);
-    }
+        public virtual void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
 
-    public virtual async Task<IEnumerable<T>> GetAllAsync()
-    {
-        return await _context.Set<T>().ToListAsync();
-    }
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
 
-    public Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, string search)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+            //return (IEnumerable<T>)await _context.Paises.FromSqlRaw("SELECT * FROM pais").ToListAsync();
+        }
 
-    public virtual async Task<T> GetByIdAsync(int id)
-    {
-        return await _context.Set<T>().FindAsync(id);
-    }
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
 
-    public virtual Task<T> GetByIdAsync(string id)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual async Task<T> GetByIdAsync(string id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
 
-    public virtual void Remove(T entity)
-    {
-        _context.Set<T>().Remove(entity);
-    }
+        public virtual void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
 
-    public virtual void RemoveRange(IEnumerable<T> entities)
-    {
-        _context.Set<T>().RemoveRange(entities);
-    }
+        public virtual void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
 
-    public virtual void Update(T entity)
-    {
-        _context.Set<T>().Update(entity);
+        public virtual void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public virtual async Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(
+            int pageIndex,
+            int pageSize,
+            string _search
+        )
+        {
+            var totalRegistros = await _context.Set<T>().CountAsync();
+            var registros = await _context
+                .Set<T>()
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return (totalRegistros, registros);
+        }
     }
 }
