a2de242c33da18c00225ba48b4a48e28a24cf751
Brayan17king <xbrayansneyder@gmail.com>
2023-10-07 09:39:17 -0500

    feat: :sparkles: Implementation of http methods in the corresponding controller

--- API/Controllers/PaisController.cs
+++ API/Controllers/PaisController.cs
@@ -27,20 +27,62 @@ public class PaisController : BaseControllerApi
         return _mapper.Map<List<PaisDto>>(paises);
     }
 
+    [HttpGet("{Id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<PaisDto>> Get(int Id)
+    {
+        var pais = await _unitOfWork.Paises.GetByIdAsync(Id);
+        if (pais == null)
+        {
+            return NotFound();
+        }
+        return _mapper.Map<PaisDto>(pais);
+    }
+
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<Pais>> Post(PaisDto paisDto)
     {
         var pais = _mapper.Map<Pais>(paisDto);
-        _unitOfWork.Paises.Add(pais);
+        _unitOfWork.Paises.Add(pais); //Agregar la informacioon que ya se encuentra en Pais
         await _unitOfWork.SaveAsync();
+        if (pais == null) //Error en la peticion
+        {
+            return BadRequest();
+        }
+        paisDto.Id = pais.Id; // Asignar el Id que se genero a nuestro atributo Id
+        return CreatedAtAction(nameof(Post), new { id = paisDto.Id }, paisDto); 
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<PaisDto>> Put(int id, [FromBody] PaisDto paisDto)
+    {
+        if(paisDto == null)
+        return NotFound();
+        var paises = _mapper.Map<Pais>(paisDto);
+        _unitOfWork.Paises.Update(paises);
+        await _unitOfWork.SaveAsync();
+        return paisDto;
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var pais = await _unitOfWork.Paises.GetByIdAsync(id);
         if (pais == null)
         {
-            return BadRequest(
-            );
+            return NotFound();
         }
-        paisDto.Id = pais.Id;
-        return CreatedAtAction(nameof(Post), new { id = paisDto.Id }, paisDto);
+        _unitOfWork.Paises.Remove(pais);
+        await _unitOfWork.SaveAsync();
+        return NoContent();
     }
 }
