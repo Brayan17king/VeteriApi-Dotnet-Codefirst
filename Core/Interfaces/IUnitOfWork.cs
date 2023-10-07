4e2e4a83db272683685b819cee95eedb209c8590
Brayan17king <xbrayansneyder@gmail.com>
2023-10-07 09:15:31 -0500

    feat: :memo: Interface updates

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -7,7 +7,16 @@ namespace Core.Interfaces
 {
     public interface IUnitOfWork
     {
-        
+        public IPaisRepository Paises { get; }
+        public ICitaRepository Citas { get; }
+        public ICiudadRepository Ciudades { get; }
+        public IClienteRepository Clientes { get; }
+        public IClienteDireccionRepository ClienteDirecciones { get; }
+        public IClienteTelefonoRepository ClienteTelefonos { get; }
+        public IDepartamentoRepository Departamentos { get; }
+        public IMascotaRepository Mascotas { get; }
+        public IRazaRepository Razas { get; }
+        public IServicioRepository Servicios { get; }
         Task<int> SaveAsync();
     }
 }
