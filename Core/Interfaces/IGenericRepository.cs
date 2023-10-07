edba40355369f8ea4724d9367b6a7229c50d3a46
Brayan17king <xbrayansneyder@gmail.com>
2023-10-07 10:29:02 -0500

    feat: :memo: Migration DataBase

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -8,7 +8,7 @@ public interface IGenericRepository<T> where T : BaseEntity
     Task<T> GetByIdAsync(int Id);
     Task<IEnumerable<T>> GetAllAsync();
     IEnumerable<T> Find(Expression<Func<T, bool>> expression); //Busquedas apartir de expresiones Linq
-    //Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, string search); //segmentar la cantidad de registros que queremos en una peticion
+    Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(int pageIndex, int pageSize, string search); //segmentar la cantidad de registros que queremos en una peticion
     void Add(T entity); //agregar 
     void AddRange(IEnumerable<T> entities);
     void Remove(T entity); // Remover
